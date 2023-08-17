a53a28ac28e05835912b85a7f3db6388a507dc7d
jmeno1011 <jwhltn8282@gmail.com>
2023-08-17 18:13:54 +0900

    add button feature and that has bugged

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
-import styles from "./LoginForm.module.css"
-import {ReactComponent as GoogleIcon} from "../../assets/google-plus.svg"
+import React, { useState } from "react";
+import styles from "./LoginForm.module.css";
+import { ReactComponent as GoogleIcon } from "../../assets/google-plus.svg";
 
 export default function LoginForm() {
+  const [user, setUser] = useState(false);
+  const googleSignIn = (response: any) => {
+    setUser(true);
+    console.log("Encoded JWT ID token: " + response.credential);
+  };
+  const onLogin = () => {
+    if (window.google) {
+      window.google.accounts.id.initialize({
+        client_id: process.env.REACT_APP_CLIENT_ID,
+        callback: googleSignIn,
+      });
+    }
+  };
+  const signOut = () => {
+    window.google.accounts.id.disableAutoSelect();
+    setUser(false);
+  };
   return (
     <div className={styles.container}>
-        <h3>Sign Up</h3>
-        <div>
-            <button className={styles.loginButton}>
-                <GoogleIcon /><span>Continue with Google</span>
-            </button>
-        </div>
+      <h3>Sign Up</h3>
+      <div>
+        <button className={styles.loginButton} onClick={onLogin}>
+          <GoogleIcon />
+          <span>Continue with Google</span>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
